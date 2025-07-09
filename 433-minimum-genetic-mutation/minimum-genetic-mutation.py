a259896075ea86d5cd53cd9d9398ec6ac74921d4
Shivam Singh Rawat <76609154+Shivam05Singh11Rawat98@users.noreply.github.com>
2025-07-09 00:47:59 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (21.15%) - LeetSync

---
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        q = deque([])
+        q.append((startGene,0))
+        seen = set(bank)
+        while q:
+            gene,step = q.popleft()
+            if gene==endGene:
+                return step
+            
+            ##calculations part
+            for i in range(9):
+                for c in ['A','C','G','T']:
+                    temp_gene = gene[:i] + c + gene[i+1:]
+                    if temp_gene in seen:
+                        q.append((temp_gene,step+1))
+                        seen.remove(temp_gene)
+
+        return -1
