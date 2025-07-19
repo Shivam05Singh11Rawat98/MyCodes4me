3d49179824faf203732bab2066076aa2b1e0915f
Shivam Singh Rawat <76609154+Shivam05Singh11Rawat98@users.noreply.github.com>
2025-07-19 02:41:03 -0500

    Time: 22 ms (5.53%) | Memory: 26.4 MB (5.02%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,9 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_prof = 0
-        n = len(prices)
-        for i in range(1,n):
-            if prices[i]>prices[i-1]:
-                max_prof+=prices[i]-prices[i-1]
+        hold, not_hold = 0,1
+
+        dp = {}
+
+        dp[(-1,hold)] = -math.inf
+        dp[(-1,not_hold)] = 0
+
+        for i,price in enumerate(prices):
+
+            dp[(i,hold)] = max(dp[(i-1,hold)], dp[(i-1,not_hold)]-price)
+            dp[(i,not_hold)] = max(dp[(i-1,hold)]+price, dp[(i-1,not_hold)])
+
         
-        return max_prof
+        return dp[len(prices)-1,not_hold]
