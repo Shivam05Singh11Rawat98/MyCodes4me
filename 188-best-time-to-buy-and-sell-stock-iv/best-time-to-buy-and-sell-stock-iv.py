25e1d67e03585885aaeaec5ec963260f717ad2ad
Shivam Singh Rawat <76609154+Shivam05Singh11Rawat98@users.noreply.github.com>
2025-07-19 03:21:00 -0500

    Time: 327 ms (6.09%) | Memory: 84.1 MB (8.35%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -4,19 +4,14 @@ class Solution:
         0->-2,0->2,-2,-4,0->-1,2,-1,-2
         """
         n = len(prices)
-        cache = {}
-        def dfs(i,buy,op):
-            print(i,op)
+        mem = {}
+        def DFS(i,no_buy,op):
             if op==0 or i>=n:
                 return 0
-            if (i,buy,op) in cache:
-                return cache[(i,buy,op)]
+            if (i,no_buy,op) in mem:
+                return mem[(i,no_buy,op)]
             
-            if buy:
-                profit = max(-prices[i] + dfs(i+1, False, op-1), dfs(i+1, True, op))
-            else:
-                profit = max(prices[i] + dfs(i+1, True, op-1), dfs(i+1, False, op))
-            cache[(i, buy, op)] = profit
-            return profit
-
-        return dfs(0,True,2*k)
+            mem[(i,no_buy,op)] = max(DFS(i+1,not no_buy,op-1) + (prices[i] if no_buy else -prices[i]), DFS(i+1,no_buy,op))
+            return mem[(i,no_buy,op)]
+        
+        return DFS(0,0,2*k)
