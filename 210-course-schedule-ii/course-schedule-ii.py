e0e765693c0a32b043ec1b0b43f71976fff9998d
Shivam Singh Rawat <76609154+Shivam05Singh11Rawat98@users.noreply.github.com>
2025-07-19 18:18:08 -0500

    Time: 6 ms (31.15%) | Memory: 19.3 MB (41.19%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,20 +1,42 @@
+"""
+course_graph
+0->1,2
+1->3
+2->3
+depend_freq
+0:0
+1:0
+2:0
+3:0
+queue:
+
+course_order = [0,1,2,3]
+"""
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        courseGraph = defaultdict(list)
-        incount = [0]*numCourses
-        for c,prc in prerequisites:
-            courseGraph[prc].append(c)
-            incount[c]+=1
+        no_course=0
+        course_order = []
+        depend_freq = defaultdict(int)
+        course_graph = defaultdict(list)
+
+        for course, pre in prerequisites:
+            course_graph[pre].append(course)
+            depend_freq[course]+=1
         
-        q = [i for i in range(numCourses) if incount[i]==0]
-        res = []
-        count=0
-        while q:
-            c = q.pop(0)
-            res.append(c)
-            for i in courseGraph[c]:
-                incount[i]-=1
-                if incount[i]==0:
-                    q.append(i)
+        queue = deque()
+
+        for course in range(numCourses):
+            if depend_freq[course]==0: 
+                queue.append(course)
         
-        return res if len(res)==numCourses else []
+        while queue:
+            pre = queue.popleft()
+            course_order.append(pre)
+            no_course+=1
+            for course in course_graph[pre]:
+                depend_freq[course]-=1
+                if depend_freq[course]==0:
+                    queue.append(course)
+
+
+        return course_order if no_course==numCourses else []
