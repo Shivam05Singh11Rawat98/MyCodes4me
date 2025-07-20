0df72a892baca131e8394350e48ff6bbfbe49ca9
Shivam Singh Rawat <76609154+Shivam05Singh11Rawat98@users.noreply.github.com>
2025-07-19 21:38:51 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (7.77%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,47 +1,32 @@
 class Solution:
-    def __init__(self):
-        self.graphMap = defaultdict()
-        self.vis = set()
-        self.ans=1
-    def constGraph(self,equations, values):
-        if not equations:
-            return
-        
-        for [start,end],value in zip(equations,values):
-            if start not in self.graphMap:
-                self.graphMap[start] = [(end,value)]
-            else:
-                self.graphMap[start].append((end,value))
-            if end not in self.graphMap:
-                self.graphMap[end] = [(start,1/value)]
-            else:
-                self.graphMap[end].append((start,1/value))
-            
-            
-    def calEquationHelper(self,start,end,val,vis):
-        vis.add(start)
-        if start==end:
-            return val
-        ans = 1
-        for next,wt in self.graphMap[start]:
-            if next not in vis:
-                ans = self.calEquationHelper(next,end,val*wt,vis)
-                if ans!=-1:
-                    return ans
-
-        return -1
-
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        self.constGraph(equations,values) #constructing the Graph (bi-direct)
-        if not self.graphMap:
-            return []
-        res = []
-        for start,end in queries:
-            if start in self.graphMap and end in self.graphMap:
-                vis = set()   
-                res.append(self.calEquationHelper(start,end,1.0,vis))
-            else:
-                res.append(float(-1))
+        # construction of Graph
+        # calculations
+        # gather query output
+        div_graph = defaultdict(list)
+        for [start,end],value in zip(equations,values):
+            div_graph[start].append((end,value))
+            div_graph[end].append((start,1/value))
         
-        return res
+        def dfs(start,end,res):
+            if start==end:
+                return res
+            vis.add(start)
+            for node,val in div_graph[start]:
+                if node not in vis:
+                    ans = dfs(node,end,res*val)
+                    if ans!=-1.0:
+                        return ans
+            
+            return -1.0
+        result=[] 
+        for start,end in queries:
+            vis = set()
+            if start not in div_graph or end not in div_graph:
+                result.append(-1)
+            else:
+                ans = dfs(start,end,1.0)
+                result.append(ans)
+        
+        return result
 
