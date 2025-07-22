8983b80a24d9ecc0c583e644babe81b77efec593
Shivam Singh Rawat <76609154+Shivam05Singh11Rawat98@users.noreply.github.com>
2025-07-21 22:10:14 -0500

    Time: 7 ms (90.14%) | Memory: 20.2 MB (55.14%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,24 +3,35 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or not len(lists):
-            return None
-
-        heap=[]
+        min_heap = []
+        merged_list = ListNode(-1)
+        temp = merged_list
         for i,node in enumerate(lists):
             if node:
-                heappush(heap, (node.val,i,node))
+                heappush(min_heap,(node.val,i,node))
         
-        t = ListNode()
-        head=t
-        while heap:
-            val,i,node = heappop(heap)
-            t.next = ListNode(val)
-            t=t.next
-            node=node.next
-            if node:   
-                heappush(heap,(node.val,i,node))
-        
-        return head.next
+        while min_heap:
+            val,i,node = heappop(min_heap)
+            temp.next = ListNode(val)
+            temp = temp.next
+            if node.next:
+                heappush(min_heap,(node.next.val,i,node.next))
+
+        return merged_list.next
+
+
+"""
+l1->1->2->3
+l2->4->5->6
+l3->7->8->9
+[(2,0),(4,1),(7,8)]
+lk -> 1->2
+"""
