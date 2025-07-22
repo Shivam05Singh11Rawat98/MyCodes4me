6bafb32f1a69dd5ad8e7a9d84bbe241f6c7fb4a4
Shivam Singh Rawat <76609154+Shivam05Singh11Rawat98@users.noreply.github.com>
2025-07-22 17:36:44 -0500

    Time: 11 ms (71.14%) | Memory: 19.7 MB (8.00%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,23 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
-        max_height_l,max_height_r = [0]*n, [0]*n
-        max_l,max_r = 0,0
-        for i in range(n):
-            max_l = max(max_l,height[i])
-            max_r = max(max_r,height[n-1-i])
-            max_height_l[i] = max_l
-            max_height_r[n-1-i] = max_r
         trapped_water = 0
-        for i in range(1,n-1):
-            if max_height_l[i]>height[i] and max_height_r[i]>height[i]:
-                trapped_water+= min(max_height_l[i],max_height_r[i])-height[i]
+        stack = []
+        for i in range(n):
+            while len(stack) and height[i]>height[stack[-1]]:
+                top = stack[-1]
+                stack.pop()
+                if len(stack)==0:
+                    break
+                # current = stack[-1]
+                distance = i - stack[-1] - 1
+                water_height = min(height[i], height[stack[-1]]) - height[top]
+                trapped_water+=distance*water_height
+
+            stack.append(i)
         
         return trapped_water
-
-"""
-h = 0,2,1,0,1,3,2,1,2
-l = 1 2 2 2 2 3 3 3 3 
-r = 3 3 3 3 3 3 2 2 2 
-water=6
-"""
